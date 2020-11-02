9f0aa37b2239aa0a9ced3b1994dbbc36fe125196
MertviuAnarhist <dubrovinantonr@mail.ru>
2020-11-02 22:42:31 +0300

    added 5 lesson

---
+++ Web/lesson5(framework)/src/main/java/ru/itmo/wp/web/page/misc/HelpPage.java
@@ -0,0 +1,13 @@
+package ru.itmo.wp.web.page.misc;
+
+import ru.itmo.wp.web.exception.RedirectException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@SuppressWarnings({"unused", "RedundantSuppression"})
+public class HelpPage {
+    private void action(HttpServletRequest request, Map<String, Object> view) {
+        // No operations.
+    }
+}
