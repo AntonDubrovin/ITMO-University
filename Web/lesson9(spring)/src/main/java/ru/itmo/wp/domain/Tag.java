a16579a417884035480fed10838b86332301774e
MertviuAnarhist <dubrovinantonr@mail.ru>
2020-12-11 13:22:32 +0300

    9 hw added

---
+++ Web/lesson9(spring)/src/main/java/ru/itmo/wp/domain/Tag.java
@@ -0,0 +1,44 @@
+package ru.itmo.wp.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(
+        uniqueConstraints = @UniqueConstraint(columnNames = "name")
+)
+public class Tag {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 32)
+    private String name;
+
+    public Tag() {
+    }
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
