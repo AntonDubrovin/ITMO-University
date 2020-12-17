34a6cbabbfa81aa6027e5400c18fa182e80049bf
MertviuAnarhist <dubrovinantonr@mail.ru>
2020-12-17 21:04:28 +0300

    10hw added

---
+++ Web/lesson10(vue.js)/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <a href="#">Codeforces</a> &copy; 2020 by Mike Mirzayanov
+    <div>Total users: {{ userCount }}</div>
+    <div>Total posts: {{ postsCount }}</div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: ["userCount", "postsCount"]
+}
+</script>
+
+<style scoped>
+
+</style>
