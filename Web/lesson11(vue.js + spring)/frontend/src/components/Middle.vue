0f8e12dcbaf8708b0572628cad1c22c43cad785c
MertviuAnarhist <dubrovinantonr@mail.ru>
2020-12-25 16:13:14 +0300

    11 hw added

---
+++ Web/lesson11(vue.js + spring)/frontend/src/components/Middle.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="middle">
+        <Sidebar :posts="viewPosts"/>
+        <main>
+            <Index v-if="page === 'Index'"/>
+            <Enter v-if="page === 'Enter'"/>
+            <Register v-if="page === 'Register'"/>
+            <Users v-if="page === 'Users'"/>
+        </main>
+    </div>
+</template>
+
+<script>
+import Sidebar from "@/components/sidebar/Sidebar";
+import Index from "@/components/middle/Index";
+import Enter from "@/components/middle/Enter";
+import Register from "@/components/middle/Register";
+import Users from "@/components/middle/Users";
+
+export default {
+    name: "Middle",
+    data: function () {
+        return {
+            page: "Index"
+        }
+    },
+    components: {
+        Register,
+        Enter,
+        Index,
+        Sidebar,
+        Users
+    },
+    props: ["posts"],
+    computed: {
+        viewPosts: function () {
+            return Object.values(this.posts).sort((a, b) => b.id - a.id).slice(0, 2);
+        }
+    }, beforeCreate() {
+        this.$root.$on("onChangePage", (page) => this.page = page)
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
