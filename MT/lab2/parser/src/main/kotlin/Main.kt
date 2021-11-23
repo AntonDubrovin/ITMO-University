7618a1f2f404bf3d2e6cd376324269323abe44dd
Anton Dubrovin <44380520+AntonDubrovin@users.noreply.github.com>
2021-11-24 00:12:07 +0300

    Update Main.kt

--- MT/lab2/parser/src/main/kotlin/Main.kt
+++ MT/lab2/parser/src/main/kotlin/Main.kt
@@ -1,20 +1,20 @@
 fun main() {
-//    println("Лёгкий тест:")
-//    val testEasy = "a or b and (c xor not(d))"
-//    val inputStreamEasy = testEasy.byteInputStream()
-//    println(Parser(LexicalAnalyzer(inputStreamEasy).apply { nextToken() }).E().toExpression())
-//
-//    println()
-//    println("Сложный тест:")
-//    val testHard = "a and not (b and (b or c) and c) and (e xor not(d))"
-//    val inputStreamHard = testHard.byteInputStream()
-//    println(Parser(LexicalAnalyzer(inputStreamHard).apply { nextToken() }).E().toExpression())
-//
-//    println()
-//    println("Тест из условия:")
-//    val testCondition = "a or (((a and b) or c) xor d)"
-//    val inputStreamCondition = testCondition.byteInputStream()
-//    println(Parser(LexicalAnalyzer(inputStreamCondition).apply { nextToken() }).E().toExpression())
+   println("Лёгкий тест:")
+   val testEasy = "a or b and (c xor not(d))"
+   val inputStreamEasy = testEasy.byteInputStream()
+   println(Parser(LexicalAnalyzer(inputStreamEasy).apply { nextToken() }).E().toExpression())
+
+   println()
+   println("Сложный тест:")
+   val testHard = "a and not (b and (b or c) and c) and (e xor not(d))"
+   val inputStreamHard = testHard.byteInputStream()
+   println(Parser(LexicalAnalyzer(inputStreamHard).apply { nextToken() }).E().toExpression())
+
+   println()
+   println("Тест из условия:")
+   val testCondition = "a or (((a and b) or c) xor d)"
+   val inputStreamCondition = testCondition.byteInputStream()
+   println(Parser(LexicalAnalyzer(inputStreamCondition).apply { nextToken() }).E().toExpression())
 
     println()
     println("TEST NOT")
